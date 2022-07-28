a8c16984dd44d87350208157e60a09f88dddd38a
najat <najatt.ismail@gmail.com>
2022-07-28 10:18:15 +0300

    add song but duration is not working

--- src/components/MusicAdd.js
+++ src/components/MusicAdd.js
@@ -1,41 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+export default class CreateSong extends Component {
+    constructor(props) {
+        super(props)
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            name: '',
+            duration: ''
+        }
+    }
+    onChangeName(e) {
+      this.setState({ name: e.target.value });   
+     }
+    onChangeDuration(e) {
+      this.setState({ duration: e.target.value });   
+     }
+    onSubmit(e) {
+        e.preventDefault()
+        const SongObject = {
+            Name: this.state.name,
+            Duration: this.state.duration
+        };
+        axios.post('http://localhost:3001/music' , SongObject)
+            .then((res) => {
+                console.log(res.data)
+            }).catch((error) => {
+                console.log(error)
+            });
+        this.setState({ name: '', duration: '' })
+    }
 
-export default class PersonAdd extends React.Component {
-  state = {
-    Name: '', 
-    duration:'206'
-  }
+    render() {
+      const { name, duration } = this.state;
 
-  handleChange = event => {
-    this.setState({ Name: event.target.value });
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    const song = {
-      name: this.state.Name
-    };
-
-    axios.post('http://localhost:3001/music', { song })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
-  }
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Song Name:
-            <input type="text" name="name" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Add</button>
-        </form>
-      </div>
-    )
-  }
+        return (
+            <div className="wrapper">
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Add Song Name</label>
+                        <input type="text" value={name} onChange={this.onChangeName} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Add Song Duration</label>
+                        <input type="text" value={duration} onChange={this.onChangeDuration} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create Song" className="btn btn-success btn-block" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
